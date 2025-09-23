38538ce2248548e452248b1b983c58a7ba2df44e
girayel <elifgiray04@gmail.com>
2025-09-23 15:30:15 +0300

    Add files via upload

---
+++ python1examples.py
@@ -0,0 +1,52 @@
+#Veri tipleri
+
+
+#numaralar
+age = 50
+print(age)
+
+
+# int -> integer
+type(10)
+type(10 * 3)
+print(type(age * 5))
+
+
+#float -> floating point
+type(3.14)
+print(type(3.14))
+myFloat = 1.33
+print(type(myFloat))
+# camelCase
+# snake_case
+print(type(40 / 4))
+type(4)  #int
+type(4.0) #float
+
+
+2 ** 5 #üssü
+10 % 3 # remainder
+
+
+#String
+print("hello python")
+name = "girayel"
+print(type(name))
+
+name.capitalize() #baş harfi büyütür.
+help(name.count) #bilmediğimiz kayıtlı fonksiyonları helpile öğrenebiliriz.
+
+name.count("e") #kaç e harfivar.
+name.upper() #tüm harfleri büyütür.
+
+
+print(name.capitalize())
+print(name.count("e"))
+print(name.upper())
+
+
+len(name) #length #kaç harf var
+
+
+print("giray /nel") # /n -> satır başı atlatır.
+print("giray /tel") # \t -> tab boşluk.
